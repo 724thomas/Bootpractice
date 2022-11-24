5e05e150319cd978daf6b70cb9eae83ffc6ad035
ThomasChoi <724thomas@gmail.com>
2022-11-24 21:44:44 +0900

    Feat: todoList, todoCalendar loads data depending on user email

--- AllInOne/src/main/java/com/example/allinone/controllers/TodoCalendarController.java
+++ AllInOne/src/main/java/com/example/allinone/controllers/TodoCalendarController.java
@@ -12,12 +12,13 @@ import java.util.HashMap;
 public class TodoCalendarController {
 
     private ArrayList<CalendarDTO> calList = new ArrayList<>();
-    private ArrayList<HashMap<String,CalendarDTO>> userCalList = new ArrayList<>();
 
 
     @GetMapping("/all/schedule")
     public ArrayList<CalendarDTO> allSchedule(HttpSession session){
+        //반환할 ArrayList<CalendarDTO> resultCalList를 만든다
         ArrayList<CalendarDTO> resultCalList = new ArrayList<>();
+        //calList 에서 해당 email과 같은 dto를 resultCalList에 넣는다.
         for (CalendarDTO aDTO : calList){
             if(aDTO.getEmail().equals(session.getAttribute("email"))){
                 resultCalList.add(aDTO);
@@ -28,7 +29,9 @@ public class TodoCalendarController {
 
     @GetMapping("/put/schedule")
     public ArrayList<CalendarDTO> putSchedule(HttpSession session, CalendarDTO dto){
+        //가져온 dto에 email변수를 해당 email로 set한다.
         dto.setEmail(String.valueOf(session.getAttribute("email")));
+        //setMail한 dto를 calList에 넣는다.
         calList.add(dto);
         return calList;
     }
