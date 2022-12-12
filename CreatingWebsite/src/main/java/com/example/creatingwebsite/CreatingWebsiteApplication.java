5837f317ffd2a12ee2fd3ab3260da5999b004f43
ThomasChoi <724thomas@gmail.com>
2022-12-12 21:05:23 +0900

    Feat : Admin page Practice

---
+++ CreatingWebsite/src/main/java/com/example/creatingwebsite/CreatingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.creatingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingWebsiteApplication.class, args);
+    }
+
+}
---
+++ CreatingWebsite/src/test/java/com/example/creatingwebsite/CreatingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creatingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
